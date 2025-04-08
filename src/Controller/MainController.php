9b891163c36435293793943624216044b7ecf68f
Luna <luna@noreply.localhost>
2025-04-08 08:38:27 +0200

    Installing PHP CS Fixer

---
+++ src/Controller/MainController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+useuseSymfony\Component\HttpFoundation\Response;
+Symfony\Component\Routing\Attribute\Route;
+class MainController
+{
+    #[Route]
+    public function HomePage(): Response
+    {
+        return new Response("<strong>Hello World !</strong>");
+    }
+}
