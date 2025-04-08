12168036a622748bfd4bc49e11faf2aba842c7f2
Luna <luna@noreply.localhost>
2025-04-08 08:40:22 +0200

    Fixing MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controller;
+
 useuseSymfony\Component\HttpFoundation\Response;
 Symfony\Component\Routing\Attribute\Route;
 class MainController
@@ -7,6 +9,6 @@ class MainController
     #[Route]
     public function HomePage(): Response
     {
-        return new Response("<strong>Hello World !</strong>");
+        return new Response('<strong>Hello World !</strong>');
     }
 }
